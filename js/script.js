6dab941fba27c62078edbb956008be93792fdbe8
axelpalmlof <95085059+axelpalmlof@users.noreply.github.com>
2022-02-28 15:58:07 +0100

    Homepage fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-var menuBtn = document.getElementById('menu_right');
-var quote = document.getElementById('quote');
-menuBtn.addEventListener('click', () => {
-    if (menuBtn.classList.contains('pressed')) {
-        quote.classList.remove('pressed')
-        menuBtn.classList.remove('pressed');
-    } else {
-        quote.classList.add('pressed')
-        menuBtn.classList.add('pressed')
-    }
-})
+var overlayBtn = document.getElementsByClassName('overlay_button');
+var main_wrapper = document.getElementById('main_wrapper');
+var body = document.getElementsByTagName('body').item(0);
+console.log(overlayBtn);
+for (btn of overlayBtn) {
+    console.log(btn);
+    btn.addEventListener('click', () => {
+        if (main_wrapper.classList.contains('overlay')) {
+            main_wrapper.classList.remove('overlay')
+            // body.classList.remove('pressed');
+        } else {
+            main_wrapper.classList.add('overlay');
+            // body.classList.add('pressed');
+        }
+    })
+}

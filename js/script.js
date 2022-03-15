b1477ec1f0bea67287dd77eca89bc8e2b44e1a2d
axelpalmlof <95085059+axelpalmlof@users.noreply.github.com>
2022-03-15 08:23:21 +0100

    Menu på hoempage

--- js/script.js
+++ js/script.js
@@ -5,12 +5,6 @@ console.log(overlayBtn);
 for (btn of overlayBtn) {
     console.log(btn);
     btn.addEventListener('click', () => {
-        if (main_wrapper.classList.contains('overlay')) {
-            main_wrapper.classList.remove('overlay')
-            // body.classList.remove('pressed');
-        } else {
-            main_wrapper.classList.add('overlay');
-            // body.classList.add('pressed');
-        }
+        main_wrapper.classList.toggle('overlay')
     })
 }

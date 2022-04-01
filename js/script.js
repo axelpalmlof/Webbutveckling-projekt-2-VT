66716ca1c55bed968d29431f556060be9149628d
axelpalmlof <axelpalmlof@gmail.com>
2022-04-01 19:32:29 +0200

    Klart
    Film tillagd, javascript validering

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var overlayBtn = document.getElementsByClassName('overlay_button');
 var main_wrapper = document.getElementById('main_wrapper');
 var body = document.getElementsByTagName('body').item(0);
 console.log(overlayBtn);
-for (btn of overlayBtn) {
+for (var btn of overlayBtn) {
     console.log(btn);
     btn.addEventListener('click', () => {
-        main_wrapper.classList.toggle('overlay')
-    })
+        main_wrapper.classList.toggle('overlay');
+    });
 }
